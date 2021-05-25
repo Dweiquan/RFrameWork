3224caec0af9764eb6434b382bb48e5e65115c6e
dengweiquan02 <466@xiaoruirui>
2021-05-25 12:53:48 +0800

    Project init

---
+++ GitSDK/GitSDK/GitSDK.h
@@ -0,0 +1,18 @@
+//
+//  GitSDK.h
+//  GitSDK
+//
+//  Created by weiquan.d on 2021/5/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GitSDK.
+FOUNDATION_EXPORT double GitSDKVersionNumber;
+
+//! Project version string for GitSDK.
+FOUNDATION_EXPORT const unsigned char GitSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GitSDK/PublicHeader.h>
+
+
